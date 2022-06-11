bf9b0d7701b5cdc4049db300ca2f1166b9cd1a84
Max <maksi.burachevskiy@gmail.com>
2022-06-12 01:38:26 +0300

    Create Djiksra

--- src/main/java/navigation/Main.java
+++ src/main/java/navigation/Main.java
@@ -2,13 +2,14 @@ package navigation;
 
 import navigation.dao.impl.BusStopDAO;
 import navigation.dao.interfaces.IBusStopDAO;
+import navigation.realization.Djikstra;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        IBusStopDAO busStopDAO = new BusStopDAO();
-        busStopDAO.showAll();
+        LOGGER.info(Djikstra.countDistance(1,18)+"   \n"+Djikstra.countDistance(2,5));
+
     }
 }
