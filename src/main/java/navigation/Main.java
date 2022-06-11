4faf16fd0dcb33a89bec293f6efd7cf9e695e384
Iuliia <badmad_91@inbox.ru>
2022-06-11 22:00:23 +0300

    fix: troubles with maven

--- src/main/java/navigation/Main.java
+++ src/main/java/navigation/Main.java
@@ -1,4 +1,10 @@
 package navigation;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+    }
 }
