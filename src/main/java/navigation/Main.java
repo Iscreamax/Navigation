2fbdb271ea1a2a89963dbf43a078a73c27140e39
Dmitry <d.galenchik@gmail.com>
2022-06-11 22:37:07 +0300

    Change names of tables in sql from Capital letter

--- src/main/java/navigation/Main.java
+++ src/main/java/navigation/Main.java
@@ -1,11 +1,14 @@
 package navigation;
 
+import navigation.dao.impl.BusStopDAO;
+import navigation.dao.interfaces.IBusStopDAO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-
+        IBusStopDAO busStopDAO = new BusStopDAO();
+        busStopDAO.showAll();
     }
 }
