420c4a1e631e1e6c8008e858c0f21f905f1f74b7
Dmitry <d.galenchik@gmail.com>
2022-06-13 00:23:41 +0300

    change show routes method

--- src/main/java/navigation/dao/impl/BusStopDAO.java
+++ src/main/java/navigation/dao/impl/BusStopDAO.java
@@ -2,6 +2,7 @@ package navigation.dao.impl;
 
 import navigation.dao.connector.ConnectionPool;
 import navigation.dao.interfaces.IBusStopDAO;
+import navigation.dao.interfaces.ICityDAO;
 import navigation.dao.models.BusStop;
 import navigation.dao.models.City;
 import org.apache.logging.log4j.LogManager;
@@ -37,6 +38,8 @@ public class BusStopDAO implements IBusStopDAO {
                 busStop.setLatitude(rs.getFloat("latitude"));
                 busStop.setLongitude(rs.getFloat("longitude"));
                 city.setId(rs.getInt("cities_id"));
+                ICityDAO cityDAO = new CityDAO();
+                city.setName(cityDAO.getEntityById(rs.getInt("cities_id")).getName());
                 busStop.setCity(city);
             }
         } catch (SQLException e) {
